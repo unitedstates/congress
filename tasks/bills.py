29648ffcc2b91df4fb913d594891a26a378b1bf4
Eric Mill <konklone@gmail.com>
2012-08-25 17:35:26 -0400

    Some basic download/caching code

--- tasks/bills.py
+++ tasks/bills.py
@@ -1,23 +1,30 @@
 import utils
 from utils import log
-
+import os
 
 def run(options):
   session = options.get('session', utils.current_session())
   bill_id = options.get('bill_id', None)
-  
+
   if not bill_id:
     log("Provide a 'bill_id' parameter to fetch data for an individual bill.")
     return
 
-  print bill_id
-
-  
+  fetch_bill(bill_id, options)
 
 
+def fetch_bill(bill_id, options):
+  if options.get('force', False):
+    body = utils.download_now(handle_for(bill_id), cache_for(bill_id, "home.html"))
+  else:
+    body = utils.download(handle_for(bill_id), cache_for(bill_id, "home.html"))
 
+  print body
 
+def cache_for(bill_id, file):
+  bill_type, number, session = utils.split_bill_id(bill_id)
+  return "data/bills/%s/%s%s/%s" % (session, bill_type, number, file)
 
-
-def handle_for(session, bill_type, number):
-  "http://hdl.loc.gov/loc.uscongress/legislation.%i%s%i" % (session, bill_type, number)
+def handle_for(bill_id):
+  bill_type, number, session = utils.split_bill_id(bill_id)
+  return "http://hdl.loc.gov/loc.uscongress/legislation.%s%s%s" % (session, bill_type, number)
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import os
 import re
 import iso8601
 from dateutil import tz
 import datetime
+
+import urllib
+import urllib2
+from urllib2 import HTTPError, URLError
 
 import pprint
 
@@ -21,3 +26,31 @@ def current_session(year=None):
   if not year:
     year = datetime.datetime.now().year
   return ((year + 1) / 2) - 894
+
+def split_bill_id(bill_id):
+  return re.match("([a-z]+)(\d+)-(\d+)", bill_id).groups()
+
+def download(url, destination):
+  if os.path.exists(destination):
+    log("Cached: %s" % url)
+    with open(destination, 'r') as f:
+      return f.read()
+  else:
+    return download_now(url, destination)
+
+def download_now(url, destination):
+  try:
+    log("Downloading: %s" % url)
+    response = urllib2.urlopen(url)
+    body = response.read()
+  except URLError, e:
+    log(e.reason)
+    return None
+
+  # cache content to disk
+  os.makedirs(os.path.dirname(destination))
+  f = open(destination, 'w')
+  f.write(body)
+  f.close()
+
+  return body
