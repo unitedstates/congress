da5479f4db905ea632009728864793812d56be81
Eric Mill <konklone@gmail.com>
2012-09-08 16:30:00 -0400

    Fix failing test since switching to use bare dates and not full timestamps when appropriate

--- test/test_bill_history.py
+++ test/test_bill_history.py
@@ -12,11 +12,14 @@ class BillHistory(unittest.TestCase):
     self.assertEqual(history['house_passage_result'], 'pass')
     self.assertEqual(self.to_date(history['house_passage_result_at']), "2010-03-21 22:48")
     self.assertEqual(history['senate_passage_result'], 'pass')
-    self.assertEqual(self.to_date(history['senate_passage_result_at']), "2009-12-24 00:00")
+    self.assertEqual(self.to_date(history['senate_passage_result_at']), "2009-12-24")
     self.assertEqual(history['vetoed'], False)
     self.assertEqual(history['awaiting_signature'], False)
     self.assertEqual(history['enacted'], True)
-    self.assertEqual(self.to_date(history["enacted_at"]), "2010-03-23 00:00")
+    self.assertEqual(self.to_date(history["enacted_at"]), "2010-03-23")
 
   def to_date(self, time):
-    return datetime.datetime.strftime(time, "%Y-%m-%d %H:%M")
+    if isinstance(time, str):
+      return time
+    else:
+      return datetime.datetime.strftime(time, "%Y-%m-%d %H:%M")
