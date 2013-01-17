unitedstates/congress

commit 4865ce2ad5704dfacf6977995becdd5eb15e8253

Drastically simplified fast-caching process for bills, by handling all cache detection in the bill pagination process. This allows for the possibility that we could overlook a change if the script aborted between caching the new state and completing the bill fetching/output process.

tasks/bills.py
@@ -15,7 +15,7 @@ def run(options):
     to_fetch = [bill_id]
   else:
     congress = options.get('congress', utils.current_congress())
-    to_fetch, to_fetch_info = bill_ids_for(congress, options, False)
+    to_fetch = bill_ids_for(congress, options, False)
     if not to_fetch:
       if options.get("fast", False):
         logging.warn("No bills changed.")
@@ -29,7 +29,7 @@ def run(options):
 
   logging.warn("Going to fetch %i bills from congress #%s" % (len(to_fetch), congress))
   
-  utils.process_set(to_fetch, bill_info.fetch_bill, options, to_fetch_info)
+  utils.process_set(to_fetch, bill_info.fetch_bill, options)
 
 
 # page through listings for bills of a particular congress
@@ -41,8 +41,6 @@ def bill_ids_for(congress, options, doing_amendments):
     bill_types = [bill_type]
   else:
     bill_types = utils.thomas_types.keys()
-    
-  bill_states = { }
     
   for bill_type in bill_types:
     # This sub is re-used for pulling amendment IDs too.
@@ -79,19 +77,21 @@ def bill_ids_for(congress, options, doing_amendments):
         bill_id = "%s-%s" % (code, congress)
         
         if options.get("fast", False):
+          fast_cache_path = utils.cache_dir() + "/" + bill_info.bill_cache_for(bill_id, "search_result.html")
+          old_state = utils.read(fast_cache_path)
+
           # Compare all of the output in the search result's <p> tag, which
           # has last major action, number of cosponsors, etc. to a cache on
           # disk to see if any major information about the bill changed.
           parent_node = link.getparent() # the <p> tag containing the whole search hit
           parent_node.remove(parent_node.xpath("b")[0]) # remove the <b>###.</b> node that isn't relevant for comparison
-          current_state = etree.tostring(parent_node) # serialize this tag
-          fast_cache_file = utils.cache_dir() + "/" + bill_info.bill_cache_for(bill_id, "search_result") # cached version
-          if os.path.exists(fast_cache_file):
-            with open(fast_cache_file) as f:
-              if f.read() == current_state:
-                logging.info("No change in search result listing: %s" % bill_id)
-                continue
-          bill_states[bill_id] = current_state
+          new_state = etree.tostring(parent_node) # serialize this tag
+
+          if old_state == new_state:
+            logging.info("No change in search result listing: %s" % bill_id)
+            continue
+          else:
+            utils.write(new_state, fast_cache_path)
         
         bill_ids.append(bill_id)
 
@@ -104,7 +104,7 @@ def bill_ids_for(congress, options, doing_amendments):
       if offset > 100000:
         break
 
-  return utils.uniq(bill_ids), bill_states
+  return utils.uniq(bill_ids)
 
 
 

tasks/bill_info.py
@@ -19,7 +19,7 @@ def run(options):
 
 # download and cache landing page for bill
 # can raise an exception under various conditions
-def fetch_bill(bill_id, options, bill_id_search_result_state=None):
+def fetch_bill(bill_id, options):
   logging.info("\n[%s] Fetching..." % bill_id)
   
   # fetch committee name map, if it doesn't already exist
@@ -40,7 +40,6 @@ def fetch_bill(bill_id, options, bill_id_search_result_state=None):
 
   if reserved_bill(body):
     logging.warn("[%s] Reserved bill, not real, skipping..." % bill_id)
-    output_bill_cached_search_result(bill_id, bill_id_search_result_state)
     return {'saved': False, 'ok': True, 'reason': "reserved bill"}
 
   # conditions where we want to parse the bill from multiple pages instead of one:
@@ -66,7 +65,6 @@ def fetch_bill(bill_id, options, bill_id_search_result_state=None):
     bill = parse_bill(bill_id, body, options)
 
   output_bill(bill, options)
-  output_bill_cached_search_result(bill_id, bill_id_search_result_state)
 
   return {'ok': True, 'saved': True}
 
@@ -337,13 +335,6 @@ def sponsor_for(body):
       }
   else:
     raise Exception("Choked finding sponsor information.")
-
-def output_bill_cached_search_result(bill_id, state):
-  # If we've been passed bill_id_search_result_state, it is a dict containing the
-  # original search result listing string, which we use for --fast mode parsing.
-  # Cache this string to disk once we do a successful parse.
-  if state:
-  	utils.write(state[bill_id], utils.cache_dir() + "/" + bill_cache_for(bill_id, "search_result"))
 
 def summary_for(body):
   match = re.search("SUMMARY AS OF:</a></b>(.*?)(?:<hr|<div id=\"footer\">)", body, re.S)
@@ -1341,4 +1332,3 @@ def bill_url_for(bill_id, page = "L"):
 def bill_cache_for(bill_id, file):
   bill_type, number, congress = utils.split_bill_id(bill_id)
   return "%s/bills/%s/%s%s/%s" % (congress, bill_type, bill_type, number, file)
-  

tasks/utils.py
@@ -150,6 +150,11 @@ def write(content, destination):
   f.write(content)
   f.close()
 
+def read(destination):
+  if os.path.exists(destination):
+    with open(destination) as f:
+      return f.read()
+
 # de-dupe a list, taken from:
 # http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
 def uniq(seq):

commit 30944696e25744810571208256144f0a895f1ede

make the --fast logic work with amendments too (broken since 4865ce2ad5704dfacf6977995becdd5eb15e8253)

tasks/amendments.py
@@ -1,21 +1,26 @@
 import utils
 import logging
 
-from bills import bill_ids_for
+from bills import bill_ids_for, save_bill_search_state
 
 from amendment_info import fetch_amendment
 
 def run(options):
   amdt_id = options.get('amendment_id', None)
+  
+  search_state = { }
 
   if amdt_id:
     amdt_type, number, congress = utils.split_bill_id(amdt_id)
     to_fetch = [amdt_id]
   else:
     congress = options.get('congress', utils.current_congress())
-    to_fetch, to_fetch_info = bill_ids_for(congress, options, doing_amendments=True)
+    to_fetch = bill_ids_for(congress, options, doing_amendments=True, bill_states=search_state)
     if not to_fetch:
-      logging.error("Error figuring out which amendments to download, aborting.")
+      if options.get("fast", False):
+        logging.warn("No amendments changed.")
+      else:
+        logging.error("Error figuring out which amendments to download, aborting.")
       return None
 
     limit = options.get('limit', None)
@@ -27,6 +32,6 @@ def run(options):
 
   logging.warn("Going to fetch %i amendments from congress #%s" % (len(to_fetch), congress))
   
-  utils.process_set(to_fetch, fetch_amendment, options)
+  saved_amendments = utils.process_set(to_fetch, fetch_amendment, options)
 
-
+  save_bill_search_state(saved_amendments, search_state)

tasks/bills.py
@@ -33,15 +33,7 @@ def run(options):
   
   saved_bills = utils.process_set(to_fetch, bill_info.fetch_bill, options)
   
-  # For --fast mode, cache the current search result listing (in search_state)
-  # to disk so we can detect major changes to the bill through the search
-  # listing rather than having to parse the bill.
-  for bill_id in saved_bills:
-    if bill_id in search_state:
-      fast_cache_path = utils.cache_dir() + "/" + bill_info.bill_cache_for(bill_id, "search_result.html")
-      new_state = search_state[bill_id]
-      utils.write(new_state, fast_cache_path)
-
+  save_bill_search_state(saved_bills, search_state)
 
 # page through listings for bills of a particular congress
 def bill_ids_for(congress, options, doing_amendments, bill_states={}):
@@ -117,7 +109,15 @@ def bill_ids_for(congress, options, doing_amendments, bill_states={}):
 
   return utils.uniq(bill_ids)
 
-
+def save_bill_search_state(saved_bills, search_state):
+  # For --fast mode, cache the current search result listing (in search_state)
+  # to disk so we can detect major changes to the bill through the search
+  # listing rather than having to parse the bill.
+  for bill_id in saved_bills:
+    if bill_id in search_state:
+      fast_cache_path = utils.cache_dir() + "/" + bill_info.bill_cache_for(bill_id, "search_result.html")
+      new_state = search_state[bill_id]
+      utils.write(new_state, fast_cache_path)
 
 def page_for(congress, bill_type, offset):
   thomas_type = utils.thomas_types[bill_type][0]