e78ab0805baa5d6fd998d78444610e2f612f1921
Eric Mill <eric@konklone.com>
2013-09-03 18:52:50 -0400

    Change up output format of nominations scraper, for consistency and a bit more detail

--- tasks/nomination_info.py
+++ tasks/nomination_info.py
@@ -10,7 +10,7 @@ from lxml.html import fromstring
 # can be run on its own, just require a nomination_id (e.g. PN2094-112)
 def run(options):
   nomination_id = options.get('nomination_id', None)
-  
+
   if nomination_id:
     result = fetch_nomination(nomination_id, options)
     logging.warn("\n%s" % result)
@@ -20,15 +20,15 @@ def run(options):
 # download and cache page for nomination
 def fetch_nomination(nomination_id, options={}):
   logging.info("\n[%s] Fetching..." % nomination_id)
-  
+
   # fetch committee name map, if it doesn't already exist
   nomination_type, number, congress = utils.split_nomination_id(nomination_id)
   if not number:
     return {'saved': False, 'ok': False, 'reason': "Couldn't parse %s" % nomination_id }
-  
+
   # fetch bill details body
   body = utils.download(
-    nomination_url_for(nomination_id), 
+    nomination_url_for(nomination_id),
     nomination_cache_for(nomination_id, "information.html"), options)
 
   if not body:
@@ -42,18 +42,18 @@ def fetch_nomination(nomination_id, options={}):
   ## detect group nominations, particularly for military promotions
   ## detect when a group nomination is split into sub nominations because of divergent Senate action
   '''
- 
+
   nomination = parse_nomination(nomination_id, body, options)
   output_nomination(nomination, options)
   return {'ok': True, 'saved': True}
-    
+
 def parse_nomination(nomination_id, body, options):
   nomination_type, number, congress = utils.split_nomination_id(nomination_id)
-  
-  #remove (and store) comments, which contain some info for the nomination but also mess up the parser  
+
+  #remove (and store) comments, which contain some info for the nomination but also mess up the parser
   facts = re.findall("<!--(.+?)-->", body)
   body = re.sub("<!--.+?-->", "", body)
-  
+
   doc = fromstring(body)
   info = { 'nomination_id': nomination_id, 'actions': [] }
 
@@ -61,44 +61,89 @@ def parse_nomination(nomination_id, body, options):
   for pair in doc.xpath('//span[@class="elabel"]|//strong'):
     if pair.tail:
         label, data = pair.text.replace(':', '').strip(), pair.tail.strip()
+
+        # handle actions separately
         if label.split(" ")[-1] == "Action":
-            data = re.split("\s+\-\s+", data)
-            info['actions'].append((label, data[0], data[1]))
+            pieces = re.split("\s+\-\s+", data)
+
+            location = label.split(" ")[0].lower()
+
+            # use 'acted_at', even though it's always a date, to be consistent
+            # with acted_at field on bills and amendments
+            acted_at = datetime.strptime(pieces[0], "%B %d, %Y").strftime("%Y-%m-%d")
+
+            # join rest back together (in case action itself has a hyphen)
+            text = str.join(" - ", pieces[1:len(pieces)])
+
+            info['actions'].append({
+              "type": "action",
+              "location": location,
+              "acted_at": acted_at,
+              "text": text
+            })
+
         else:
-            info[label.lower()] = data
+            # let's handle these cases one by one
+            if label == "Organization":
+              info["organization"] = data
+
+            elif label == "Control Number":
+              info["control_number"] = data
+
+            elif label == "Referred to":
+              info["referred_to"] = data
+
+            elif label == "Reported by":
+              info["reported_by"] = data
+
+            elif label == "Nomination":
+              # sanity check - verify nomination_id matches
+              if nomination_id != data:
+                raise Exception("Whoa! Mismatched nomination ID.")
+
+            elif label == "Date Received":
+              # Note: Will break with the 1000th congress in year 3789
+              match = re.search("(\d{2,3})[stndhr]{2}", data)
+              if match:
+                info["congress"] = int(match.group(1))
+              else:
+                raise Exception("Choked, couldn't find Congress in \"%s\"" % data)
+
+              # Doc format is: "January 04, 1995 (104th Congress)"
+              info["received_on"] = datetime.strptime(data.split(" (")[0], "%B %d, %Y").strftime("%Y-%m-%d")
+
+            elif label == "Nominee":
+              # remove final clause if there
+              info["nominee"] = data.split(", vice")[0]
+            else:
+              logging.info("Unrecognized label: %s" % label)
 
   '''
   Some of the data is structured fine as is (e.g. Organization, Referred to, Reported by)
   Some needs processing, like date and nominee
   '''
-  
-  # Doc format is: "January 04, 1995 (104th Congress)"
-  info["date"] = datetime.strptime(info["date received"].split(" (")[0], "%B %d, %Y").strftime("%Y-%m-%d")
-  # Note: Will break with the 1000th congress in year 3789
-  info["congress"] = int(re.search("(\d{2,3})[stndhr]{2}", info["date received"]).group(1))
-  
-  # remove final caluse if there
-  info["nominee"] = info["nominee"].split(", vice")[0]
-  
+
+  if not info.get("received_on", None):
+    raise Exception("Choked, couldn't find received date.")
+
+  if not info.get("nominee", None):
+    raise Exception("Choked, couldn't find nominee info.")
+
   # get overview from the text of the nomination
   try:
     (name, state, position) = re.search("(.+?), of (.+?), to be (.+?)", info["nominee"]).groups()
   except Exception, e:
-    logging.error("Couldn't parse %s" % info["nominee"])
-    (name, state, position) = ("", "", "")    
-    
+    raise Exception("Couldn't parse %s" % info["nominee"])
+
   info["name"] = name
-  info["state"] = re.sub("^the ", "", state) #the District -> District
-  info["parsed"] = ""
-  info["position"] = ""
-  info["st_abbr"] = ""
+  info["state_name"] = re.sub("^the ", "", state) #the District -> District
 
   # if we want to test whether the info in the comments align with the info gleaned from text, can ask if name == facts[-3]
-  #doesn't handle suffixes at the moment
-  info["parsed"] = re.search("([A-z-'\s,\.]+),\s([A-z-\']+)([A-z-\'\.\s]*)", facts[-4]).groups()
+  # doesn't handle suffixes at the moment
+  # info["parsed"] = re.search("([A-z-'\s,\.]+),\s([A-z-\']+)([A-z-\'\.\s]*)", facts[-4]).groups()
   info["position"] = facts[-5]
-  info["st_abbr"] = facts[-6][2:]
-  
+  info["state"] = facts[-6][2:]
+
   return info
 
 # directory helpers
@@ -119,6 +164,6 @@ def output_nomination(nomination, options):
 
   # output JSON - so easy!
   utils.write(
-    json.dumps(nomination, sort_keys=True, indent=2, default=utils.format_datetime), 
+    json.dumps(nomination, sort_keys=True, indent=2, default=utils.format_datetime),
     output_for_nomination(nomination['nomination_id'], "json")
   )
