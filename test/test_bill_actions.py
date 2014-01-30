62b8aca1a88648db7a3e1ddac68ec0830d9f07e7
Joshua Tauberer <jt@occams.info>
2014-01-30 15:08:07 -0500

    tests broke in 3922aa766d667f33d296a61e73658fa79dd15108 because I changed the call signature of bill_info.parse_bill_action

--- test/test_bill_actions.py
+++ test/test_bill_actions.py
@@ -3,6 +3,9 @@ import bill_info
 
 # parsing various kinds of action text to extract metadata and establish state
 
+def parse_bill_action(line, state, bill_id, title):
+  return bill_info.parse_bill_action( { "text": line }, state, bill_id, title)
+
 class BillActions(unittest.TestCase):
 
   def test_veto(self):
@@ -11,7 +14,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Vetoed by President."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vetoed")
     self.assertEqual(new_state, "PROV_KILL:VETO")
 
@@ -21,7 +24,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Pocket Vetoed by President."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vetoed")
     self.assertEqual(new_action['pocket'], "1")
     self.assertEqual(new_state, "VETOED:POCKET")
@@ -32,7 +35,7 @@ class BillActions(unittest.TestCase):
     state = "REFERRED"
     line = "Committee on the Judiciary. Ordered to be reported with an amendment in the nature of a substitute favorably."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], 'calendar')
     # self.assertEqual(new_action['committee'], "Committee on the Judiciary")
@@ -44,7 +47,7 @@ class BillActions(unittest.TestCase):
     state = "REPORTED"
     line = "Placed on Senate Legislative Calendar under General Orders. Calendar No. 70."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], 'calendar')
     self.assertEqual(new_action['calendar'], "Senate Legislative")
@@ -58,7 +61,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Became Public Law No: 111-148."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "enacted")
     self.assertEqual(new_action['congress'], "111")
     self.assertEqual(new_action['number'], "148")
@@ -70,7 +73,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Cleared for White House."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     # should not be marked as presented to president, since it hasn't been yet
     # self.assertEqual(new_action['type'], 'action')
@@ -81,7 +84,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Presented to President."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], 'topresident')
 
   def test_signed_by_president(self):
@@ -90,7 +93,7 @@ class BillActions(unittest.TestCase):
     state = "PASSED:BILL"
     line = "Signed by President."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], 'signed')
 
 
@@ -102,7 +105,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "On motion to suspend the rules and pass the bill Agreed to by the Yeas and Nays: (2/3 required): 416 - 0 (Roll no. 768)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
@@ -119,7 +122,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_OVER:HOUSE"
     line = "Passed Senate with an amendment and an amendment to the Title by Yea-Nay Vote. 60 - 39. Record Vote Number: 396."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote2")
@@ -136,7 +139,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_BACK:SENATE"
     line = "On motion that the House agree to the Senate amendments Agreed to by recorded vote: 219 - 212 (Roll no. 165)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['roll'], "165")
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "pingpong")
@@ -150,7 +153,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_OVER:HOUSE" # should not change
     line = "Cloture on the motion to proceed to the bill invoked in Senate by Yea-Nay Vote. 60 - 39. Record Vote Number: 353."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['roll'], "353")
     self.assertEqual(new_action['type'], "vote-aux")
     self.assertEqual(new_action['vote_type'], "cloture")
@@ -166,7 +169,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_OVER:HOUSE" # should not change
     line = "Cloture invoked in Senate by Yea-Nay Vote. 60 - 39. Record Vote Number: 395."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['roll'], "395")
     self.assertEqual(new_action['type'], "vote-aux")
     self.assertEqual(new_action['vote_type'], "cloture")
@@ -185,7 +188,7 @@ class BillActions(unittest.TestCase):
   #   state = "PASS_OVER:HOUSE" # should not change
   #   line = "Motion to proceed to consideration of measure agreed to in Senate by Unanimous Consent."
 
-  #   new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+  #   new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
   #   self.assertEqual(new_action['type'], 'vote')
   #   self.assertEqual(new_action['vote_type'], 'other')
@@ -200,7 +203,7 @@ class BillActions(unittest.TestCase):
   #   state = "PASS_OVER:HOUSE" # should not change
   #   line = "Motion by Senator McCain to commit to Senate Committee on Finance under the order of 12/2/2009, not having achieved 60 votes in the affirmative, the motion was rejected in Senate by Yea-Nay Vote. 42 - 58. Record Vote Number: 358."
 
-  #   new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+  #   new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
   #   self.assertEqual(new_action['type'], 'vote')
   #   self.assertEqual(new_action['vote_type'], 'other')
@@ -216,7 +219,7 @@ class BillActions(unittest.TestCase):
   #   state = "PASS_BACK:SENATE"
   #   line = "On motion that the House disagree to the Senate amendments, and request a conference Agreed to by the Yeas and Nays: 229 - 193 (Roll no. 946)."
 
-  #   new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+  #   new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
   #   # self.assertEqual(new_action['type'], 'vote')
   #   # self.assertEqual(new_action['vote_type'], 'other')
@@ -232,7 +235,7 @@ class BillActions(unittest.TestCase):
   #   state = "PASS_BACK:SENATE"
   #   line = "On motion that the House instruct conferees Agreed to by the Yeas and Nays: 397 - 16 (Roll no. 9)."
 
-  #   new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+  #   new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
   #   # self.assertEqual(new_action['type'], 'vote')
   #   # self.assertEqual(new_action['vote_type'], 'other')
@@ -248,7 +251,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_BACK:SENATE"
     line = "On agreeing to the conference report Agreed to by the Yeas and Nays: 293 - 132 (Roll no. 72)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], 'vote')
     self.assertEqual(new_action['vote_type'], 'conference')
@@ -264,7 +267,7 @@ class BillActions(unittest.TestCase):
     state = "CONFERENCE:PASSED:HOUSE"
     line = "Senate agreed to conference report by Yea-Nay Vote. 60 - 36. Record Vote Number: 22."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], 'vote')
     self.assertEqual(new_action['vote_type'], 'conference')
@@ -281,7 +284,7 @@ class BillActions(unittest.TestCase):
     state = "PROV_KILL:VETO"
     line = "On passage, the objections of the President to the contrary notwithstanding Failed by the Yeas and Nays: (2/3 required): 143 - 245, 1 Present (Roll no. 2).On passage, the objections of the President to the contrary notwithstanding Failed by the Yeas and Nays: (2/3 required): 143 - 245, 1 Present (Roll no. 2)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "override")
     self.assertEqual(new_action['where'], "h")
@@ -296,7 +299,7 @@ class BillActions(unittest.TestCase):
     state = "PROV_KILL:VETO"
     line = "Two-thirds of the Members present having voted in the affirmative the bill is passed, Passed by the Yeas and Nays: (2/3 required): 383 - 41 (Roll no. 491)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "override")
     self.assertEqual(new_action['where'], "h")
@@ -311,7 +314,7 @@ class BillActions(unittest.TestCase):
     state = "VETOED:OVERRIDE_PASS_OVER:HOUSE"
     line = "Passed Senate over veto by Yea-Nay Vote. 70 - 26. Record Vote Number: 177."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "override")
     self.assertEqual(new_action['where'], "s")
@@ -331,7 +334,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_OVER:SENATE"
     line = "On passage Passed by the Yeas and Nays: (2/3 required): 286 - 130 (Roll no. 296)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote2")
     self.assertEqual(new_action['where'], "h")
@@ -346,7 +349,7 @@ class BillActions(unittest.TestCase):
     state = "PASS_OVER:HOUSE"
     line = "Received in the Senate, considered, and agreed to without amendment by Unanimous Consent."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote2")
     self.assertEqual(new_action['where'], "s")
@@ -360,7 +363,7 @@ class BillActions(unittest.TestCase):
     state = "REPORTED"
     line = "On agreeing to the resolution, as amended Agreed to by the Yeas and Nays: 253 - 175 (Roll no. 16)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
     self.assertEqual(new_action['where'], "h")
@@ -375,7 +378,7 @@ class BillActions(unittest.TestCase):
     state = "REPORTED"
     line = "Submitted in the Senate, considered, and agreed to without amendment and with a preamble by Unanimous Consent."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
     self.assertEqual(new_action['where'], "s")
@@ -389,7 +392,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Disagreed to in Senate by Voice Vote."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
     self.assertEqual(new_action['where'], "s")
@@ -403,7 +406,7 @@ class BillActions(unittest.TestCase):
     state = "REFERRED"
     line = "On motion to suspend the rules and pass the bill Failed by the Yeas and Nays: (2/3 required): 97 - 318, 7 Present (Roll no. 379)."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
     self.assertEqual(new_action['where'], "h")
@@ -418,7 +421,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Passed House pursuant to H. Res. 241."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
     self.assertEqual(new_action['type'], "vote")
     self.assertEqual(new_action['vote_type'], "vote")
     self.assertEqual(new_action['where'], "h")
@@ -434,7 +437,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Referred to House Homeland Security"
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertIn("committees", new_action)
     self.assertEqual(new_action['committees'], ["HSHM"])
@@ -445,7 +448,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Referred to the House Committee on Financial Services."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertIn("committees", new_action)
     self.assertEqual(new_action['committees'], ["HSBA"])
@@ -456,7 +459,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Committee on Appropriations. Original measure reported to Senate by Senator Mikulski. With written report No. 113-78."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     # it's a Senate bill, so we assume un-chamber-ed committee refs are to a Senate committee
     self.assertIn("committees", new_action)
@@ -468,7 +471,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Referred to the Committee on Homeland Security, and in addition to the Committees on Armed Services, Agriculture, and Natural Resources, for a period to be subsequently determined by the Speaker, in each case for consideration of such provisions as fall within the jurisdiction of the committee concerned."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], "referral")
     self.assertEqual(new_state, "REFERRED")
@@ -479,7 +482,7 @@ class BillActions(unittest.TestCase):
     state = "INTRODUCED"
     line = "Referred to the Subcommittee Indian and Alaska Native Affairs."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], "referral")
     self.assertEqual(new_state, "REFERRED")
@@ -490,7 +493,7 @@ class BillActions(unittest.TestCase):
     state = "REFERRED"
     line = "Committee on the Judiciary Subcommittee on the Constitution, Civil Rights and Human Rights. Hearings held."
 
-    new_action, new_state = bill_info.parse_bill_action(line, state, bill_id, title)
+    new_action, new_state = parse_bill_action(line, state, bill_id, title)
 
     self.assertEqual(new_action['type'], "hearings")
     # self.assertEqual(new_action['committees'], "Committee on the Judiciary Subcommittee on the Constitution, Civil Rights and Human Rights")
