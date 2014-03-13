unitedstates/congress

commit 9b01c418c2c29e14c4a25903396a856b27ec9b42

when mirroring FDSys it is considerably faster (and less memory intensive) to shell out to wget to do the download rather than using scrapelib (but what about throttling?)

tasks/utils.py
@@ -152,6 +152,9 @@ def download(url, destination=None, options={}):
   # if need a POST request with data
   postdata = options.get('postdata', False)
 
+  # caller cares about actually bytes or only success/fail
+  needs_content = options.get('needs_content', True) or not xml or postdata
+
   if test:
     cache = test_cache_dir()
   else:
@@ -165,6 +168,7 @@ def download(url, destination=None, options={}):
 
   if destination and (not force) and os.path.exists(cache_path):
     if not test: logging.info("Cached: (%s, %s)" % (cache, url))
+    if not needs_content: return True
     with open(cache_path, 'r') as f:
       body = f.read()
   else:
@@ -174,6 +178,10 @@ def download(url, destination=None, options={}):
       if postdata:
         response = scraper.urlopen(url, 'POST', postdata)
       else:
+        if not needs_content:
+          import subprocess
+          mkdir_p(os.path.dirname(cache_path))
+          return True if (subprocess.call(["wget", "-q", "-O", cache_path, url]) == 0) else None
         response = scraper.urlopen(url)
       body = response.bytes # str(...) tries to encode as ASCII the already-decoded unicode content
     except scrapelib.HTTPError as e:

tasks/fdsys.py
@@ -312,7 +312,8 @@ def mirror_file(year, collection, package_name, lastmod, granule_name, file_type
     data = utils.download(f_url, f_path, utils.merge(options, {
       'xml': True, 
       'force': force, 
-      'to_cache': False
+      'to_cache': False,
+      'needs_content': file_type == "text" and f_path.endswith(".html"),
     }))
     
     if not data:

commit 7c21b296e4c81aa0a001b56d5285cfd090fa3830

don't try to use wget to download from FDSys if wget isn't available
If the system doesn't have wget, was broken by 9b01c418c2c29e14c4a25903396a856b27ec9b42.

Hopefully fixes the second issue in #113.

tasks/utils.py
@@ -267,16 +267,28 @@ def download(url, destination=None, options={}):
       if postdata:
         response = scraper.urlopen(url, 'POST', postdata)
       else:
-        if not needs_content:
+
+        # If we're just downloading the file and the caller doesn't
+        # need the response data, then starting wget to download the
+        # file is much faster for large files. Don't know why. Something
+        # hopefully we can improve in scrapelib in the future.
+        #
+        # needs_content is currently only set to false when downloading
+        # bill text files like PDFs.
+        #
+        # Skip this fast path if wget is not present in its expected location.
+        wget_path = "/usr/bin/wget"
+        if not needs_content and os.path.exists(wget_path):
           import subprocess
           mkdir_p(os.path.dirname(cache_path))
-          if subprocess.call(["wget", "-q", "-O", cache_path, url]) == 0:
+          if subprocess.call([wget_path, "-q", "-O", cache_path, url]) == 0:
             return True
           else:
             # wget failed. when that happens it leaves a zero-byte file on disk, which
             # for us means we've created an invalid file, so delete it.
             os.unlink(cache_path)
             return None
+
         response = scraper.urlopen(url)
 
       if not is_binary: