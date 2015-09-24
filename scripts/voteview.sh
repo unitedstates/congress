9c809575e5fddbbad278734f65444327a2870931
Joshua Tauberer <jt@occams.info>
2015-09-24 16:07:03 -0400

    in voteview.sh, update congress-legislators in first iteration but not after

--- scripts/voteview.sh
+++ scripts/voteview.sh
@@ -1,6 +1,8 @@
-export UPDATE_CONGRESS_LEGISLATORS=NO
 for congress in {1..100}; do
 	./run voteview --congress=$congress --govtrack $@
+
+    # After the first run, no need to update legislator info.
+    export UPDATE_CONGRESS_LEGISLATORS=NO
 done
 ./run voteview --govtrack --congress=101 --session=1989 --chamber=h $@
 
