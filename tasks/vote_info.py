4dcfcc5403889c4d6c53242e647c105e42cff833
Joshua Tauberer <jt@occams.info>
2015-11-02 15:18:50 -0500

    correct vote category parse in h579-114.2015 --- this appears to be the first vote to have this particular issue

--- tasks/vote_info.py
+++ tasks/vote_info.py
@@ -498,7 +498,7 @@ def get_vote_category(vote_question):
         (r"^On the Motion \(Motion to Concur", "passage"),
 
         # house only
-        (r"^(On Motion to )?(Concur in|Concurring|Concurring in|On Concurring|Agree to|On Agreeing to) (the )?Senate (Amendment|amdt|Adt)s?", "passage"),
+        (r"^(On Motion (to|that the House) )?(Concur in|Concurring|Concurring in|On Concurring|Agree to|On Agreeing to) (the )?Senate (Amendment|amdt|Adt)s?", "passage"),
         (r"^(On Motion to )?Suspend (the )?Rules and (Agree|Concur|Pass)", "passage-suspension"),
         (r"^Call of the House$", "quorum"),
         (r"^Call by States$", "quorum"),
