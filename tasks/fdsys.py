unitedstates/congress

commit deab2f384df0db2cc682c12440da65c9c7c2682f

Use urlretrieve() instead of wget, speed up FDsys

tasks/fdsys.py
@@ -402,8 +402,7 @@ def mirror_package_or_granule(sitemap, package_name, granule_name, lastmod, opti
             'force': True, # decision to cache was made above
             'to_cache': False,
             'return_status_code_on_error': True,
-            # an old optimization, but it conflicts with return_status_code_on_error
-            #'needs_content': (file_type == "text" and file_path.endswith(".html")),
+            'needs_content': (file_type == "text" and file_path.endswith(".html")),
         }))
 
         # Download failed?

tasks/utils.py
@@ -307,32 +307,10 @@ def download(url, destination=None, options={}):
             if postdata:
                 response = scraper.urlopen(url, 'POST', postdata, **urlopen_kwargs)
             else:
-
-                # If we're just downloading the file and the caller doesn't
-                # need the response data, then starting wget to download the
-                # file is much faster for large files. Don't know why. Something
-                # hopefully we can improve in scrapelib in the future.
-                #
-                # needs_content is currently only set to false when downloading
-                # bill text files like PDFs.
-                #
-                # Skip this fast path if wget is not present in its expected location.
-                with open(os.devnull, 'w') as tempf:
-                    if platform.system() == 'Windows':
-                        wget_exists = (subprocess.call("where wget", stdout=tempf, stderr=tempf, shell=True) == 0)
-                    else:
-                        wget_exists = (subprocess.call("which wget", stdout=tempf, stderr=tempf, shell=True) == 0)
-
-                if not needs_content and wget_exists:
-
+                if not needs_content:
                     mkdir_p(os.path.dirname(cache_path))
-                    if subprocess.call(["wget", "-q", "-O", cache_path, url]) == 0:
-                        return True
-                    else:
-                        # wget failed. when that happens it leaves a zero-byte file on disk, which
-                        # for us means we've created an invalid file, so delete it.
-                        os.unlink(cache_path)
-                        return None
+                    scraper.urlretrieve(url, cache_path, **urlopen_kwargs)
+                    return True
 
                 response = scraper.urlopen(url, **urlopen_kwargs)
 

commit 236f02751236656f5565ea251e71fe9823a5068f

deab2f384df0db2cc682c12440da65c9c7c2682f broke fdsys: successfuld downloads were treated as unknown errors and files were being refetched over and over

tasks/fdsys.py
@@ -455,6 +455,12 @@ def mirror_package_or_granule(sitemap, package_name, granule_name, lastmod, opti
             elif sitemap["collection"] == "BILLS" and file_type in ("text", "mods"):
                 # expected to be present for bills
                 raise Exception("Failed to download %s %s (404)" % (package_name, file_type))
+        elif data is True:
+            # Download was successful but needs_content was False so we don't have the
+            # file content. Instead, True is returned. Strangely isintance(True, int) is
+            # True (!!!) so we have to test for True separately from testing if we got a
+            # return code integer.
+            pass
         elif not data or isinstance(data, int):
             # There was some other error - skip the rest. Don't
             # update file_lastmod!