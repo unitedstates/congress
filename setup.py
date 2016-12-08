eb886bfc36e082d985814f117e54376549ca041b
Ben Nelson <nelson.ben.c@gmail.com>
2016-12-07 23:44:01 -0700

    beginning packaging using pypi
    moving packaging to united-states-congress
      -TODO: fix specs, other paths
      -TODO: add entry_points, repackage, update version and remove run scripts
    pypi package here https://pypi.python.org/pypi/united-states-congress

---
+++ setup.py
@@ -0,0 +1,85 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+from pip.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requirements_path = path.join(here, 'requirements.txt')
+install_requirements = parse_requirements(requirements_path, session=False)
+requirements = [str(ir.req) for ir in install_requirements]
+
+setup(
+    name='united-states-congress',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='Public domain data collectors for the work of Congress, including legislation, amendments, and votes.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/unitedstates/congress',
+
+    # Author details
+    author='United States',
+    author_email='test@replaceme.com',
+
+    # Choose your license
+    license='CC0-1.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    # What does your project relate to?
+    keywords='government development data',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=requirements
+
+    # TODO: maybe in teh future?
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    #entry_points={
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+)
