d2c6b5da0cd32941429d0ecb81bc49d2a11ac02c
Ben Nelson <nelson.ben.c@gmail.com>
2016-12-10 22:40:22 -0700

    bump in version to add requirements to manifest

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.0.3',
 
     description='Public domain data collectors for the work of Congress, including legislation, amendments, and votes.',
     long_description=long_description,
