e77fad1f39cfd82830dbf507d853a74277142e37
acxz <17132214+acxz@users.noreply.github.com>
2020-10-04 11:00:07 -0400

    fix file modes

--- scripts/statutes.sh
+++ scripts/statutes.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ./run govinfo --collections=STATUTE --extract=mods,pdf
 ./run statutes --volumes=65-86 --govtrack # bill status
 ./run statutes --volumes=65-106 --textversions --extracttext # bill text
