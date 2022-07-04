a215e02f0fd24ea1d1f3ed8731b8de5bbb665d9f
connorjoleary <oleary.connor24@gmail.com>
2022-07-03 22:27:29 -0700

    add tests

--- test/test_hearing_parser.py
+++ test/test_hearing_parser.py
@@ -9,8 +9,25 @@ class TestHearingParser(unittest.TestCase):
 
     def test_regex_pattern(self):
         pattern = self.parser.regex_pattern
-        # print(re.match(new_pattern, '\n mr. doe. what are you?', flags=re.I))
         self.assertTrue(re.match(pattern, '\n mr. doe. what are you?'))
+        self.assertTrue(re.match(pattern, '\n miss piper. fuck you'))
+        # self.assertTrue(re.match(pattern, '\n the chairman. i call'))
+        self.assertTrue(re.match(pattern, '\n   dr. walensky . at'))
+
+        self.assertFalse(re.match(pattern, 'i love the\ngeneral public.'))
+        self.assertFalse(re.match(pattern, '\nDr. R. John Hansman Jr., T. Wilson Professor of Aeronautics'))
+
+    def test_clean_hearing_text(self):
+        text_with_directions = """    Mrs. Maloney. Well, what----
+    Dr. Walensky [continuing]. at CDC are deeply concerned
+        """
+        cleaned_text_with_directions = """    Mrs. Maloney. Well, what----
+    Dr. Walensky . at CDC are deeply concerned
+        """
+        self.assertEquals(self.parser.clean_hearing_text(text_with_directions), cleaned_text_with_directions)
+
+    def test_(self):
+        print('setup')
 
 if __name__ == '__main__':
     unittest.main()
