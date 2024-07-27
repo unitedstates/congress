e07e3f5fd40b33f7bd5d8da231580d8083c7421d
jerrywithaz <dev@jerrywithaz.com>
2024-07-26 21:50:36 -0500

    Update patch

--- congress/patch/patch.py
+++ congress/patch/patch.py
@@ -6,19 +6,22 @@ import boto3
 from botocore.errorfactory import ClientError
 import smart_open
 
-session = boto3.Session(profile_name='Democrasee')
-client = session.client('s3')
-transport_params = {'client': client}
-bucket = 'democrasee-storage'
+session = boto3.Session(
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+)
+client = session.client("s3")
+transport_params = {"client": client}
+bucket = "democrasee-storage"
 
 original_open = open
 original_exists = os.path.exists
 
-def open_wrapper(uri: Any, mode: str = 'r', *args):
-    if isinstance(uri, str) and uri.startswith('raw'):
+
+def open_wrapper(uri: Any, mode: str = "r", *args):
+    if isinstance(uri, str) and uri.startswith("raw"):
         s3_url = f"s3://{bucket}/{uri}"
-        file = smart_open.open(uri=s3_url, mode=mode,
-                               transport_params=transport_params)
+        file = smart_open.open(uri=s3_url, mode=mode, transport_params=transport_params)
         return file
 
     return original_open(uri, mode, *args)
@@ -26,11 +29,8 @@ def open_wrapper(uri: Any, mode: str = 'r', *args):
 
 def exists(path: Any):
     try:
-        if (isinstance(path, str) and path.startswith('raw')):
-            client.get_object(
-                Bucket=bucket,
-                Key=path
-            )
+        if isinstance(path, str) and path.startswith("raw"):
+            client.get_object(Bucket=bucket, Key=path)
 
             return True
 
@@ -40,14 +40,16 @@ def exists(path: Any):
 
 
 def cache_dir_wrapper():
-    return 'raw/congress/cache'
+    return "raw/congress/cache"
 
 
 def data_dir_wrapper():
-    return 'raw/congress/data'
+    return "raw/congress/data"
+
 
 def mkdir_p(path):
     return None
+
 
 def patch(task_name):
     govinfo.utils.mkdir_p = mkdir_p
@@ -59,5 +61,5 @@ def patch(task_name):
     bills.utils.cache_dir = cache_dir_wrapper
     bills.utils.data_dir = data_dir_wrapper
     bills.os.path.exists = exists
-    
-    __builtins__['open'] = open_wrapper
+
+    __builtins__["open"] = open_wrapper
